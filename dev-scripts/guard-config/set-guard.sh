c20479d5e48bc148a7810a6ab3a69169a0efb170
duvbell <kevindev6034@gmail.com>
2025-11-29 11:34:36 +0000

    deplyoment subvault

--- dev-scripts/guard-config/set-guard.sh
+++ dev-scripts/guard-config/set-guard.sh
@@ -1,6 +1,4 @@
-source dev-scripts/current-env.sh
-
-APPROVE_GUARD_ADDRESS=$APPROVE_GUARD_ADDRESS
+ETH_TOKEN_GUARD_ADDRESS=$ETH_TOKEN_GUARD_ADDRESS
 
 ## Set approve guard for the token address
-cast send $CURRENT_VAULT_ADDRESS "setGuard(address,address)" $CURRENT_ASSET_ADDRESS $APPROVE_GUARD_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY
+cast send $CURRENT_VAULT_ADDRESS "setGuard(address,address)" $CURRENT_ASSET_ADDRESS $ETH_TOKEN_GUARD_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY
